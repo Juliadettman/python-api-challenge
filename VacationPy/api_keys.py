b1abab9071d65e3e1736c680839042c2061ad22a
Juliadettman <143463072+Juliadettman@users.noreply.github.com>
2023-10-12 15:41:26 -0500

    Update api_keys.py

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "6656240843f5d9014199d9096f902f45"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "188cf38fe9dd47cda7b7731e671f5be5"
